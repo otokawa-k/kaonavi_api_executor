346725b2a6d2eca0e32a7d12ce50f70ea7367608
otokawa-k <kiyotaka.otokawa@tatsu-mi.co.jp>
2025-04-22 18:02:30 +0900

    アクセストークン取得処理追加

---
+++ src/kaonavi_api_executor/api_access_token_fetcher.py
@@ -0,0 +1,34 @@
+import os
+import requests
+from requests.auth import HTTPBasicAuth
+
+
+class ApiAccessTokenFetcher:
+    """
+    Fetches the access token for the Kaonavi API.
+    """
+
+    def __init__(self):
+        self.consumer_key = os.getenv('KAONAVI_CONSUMER_KEY')
+        self.consumer_secret = os.getenv('KAONAVI_CONSUMER_SECRET')
+        if not self.consumer_key or not self.consumer_secret:
+            raise ValueError("Consumer key and secret must be set in environment variables.")
+
+    def fetch_access_token(self) -> str:
+        response = requests.post(
+            'https://api.kaonavi.jp/api/v2.0/token',
+            auth=HTTPBasicAuth(self.consumer_key, self.consumer_secret),
+            data='grant_type=client_credentials',
+            headers={
+                'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'},
+        )
+        # responseをjson形式で取得
+        if response.status_code != 200:
+            raise Exception(f"Failed to fetch access token: {response.status_code} {response.text}")
+        # responseをjson形式で取得
+        response_json = response.json()
+        # access_tokenを取得
+        access_token = response_json.get('access_token')
+        if not access_token:
+            raise Exception("Access token not found in response.")
+        return access_token
---
+++ tests/conftest.py
@@ -0,0 +1,4 @@
+from dotenv import load_dotenv
+import pathlib
+
+load_dotenv(dotenv_path=pathlib.Path(".env.test"))
