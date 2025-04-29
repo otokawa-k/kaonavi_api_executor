600fb023484cb29b3acf4751f72795873e225bf2
otokawa-k <kiyotaka.otokawa@tatsu-mi.co.jp>
2025-04-29 21:43:10 +0900

    シート取得処理追加完了

---
+++ src/kaonavi_api_executor/api/get_sheets_api.py
@@ -0,0 +1,33 @@
+import os
+from typing import Any, Dict
+from pydantic import BaseModel
+from .api_model import ApiModel
+from ..http_client.http_client import HttpClient
+from ..http_client.http_methods import Get
+
+
+class SheetsResponse(BaseModel):
+    id: int
+    name: str
+    record_type: int
+    updated_at: str
+    member_data: list[Dict[str, Any]]
+
+
+class GetSheetsApi(ApiModel[SheetsResponse]):
+    def __init__(self, token: str, sheet_id: int) -> None:
+        super().__init__()
+
+        if token is None:
+            raise ValueError("Token must be provided")
+
+        url = os.getenv("KAONAVI_API_URL", "https://api.kaonavi.jp/api/v2.0")
+        self.url = f"{url}/sheets/{str(sheet_id)}"
+        self.headers = {"Content-Type": "application/json", "Kaonavi-Token": token}
+
+    @property
+    def method(self) -> HttpClient:
+        return Get()
+
+    def parse_response(self, raw_json: Dict[str, Any]) -> SheetsResponse:
+        return SheetsResponse.model_validate(raw_json)
--- tests/test_api_executor.py
+++ tests/test_api_executor.py
@@ -2,6 +2,7 @@ import pytest
 from kaonavi_api_executor.auth.api_access_token_fetcher import ApiAccessTokenFetcher
 from kaonavi_api_executor.api_executor import ApiExecutor
 from kaonavi_api_executor.api.get_members_api import GetMembersApi
+from kaonavi_api_executor.api.get_sheets_api import GetSheetsApi
 from kaonavi_api_executor.http_client.http_methods import Post
 from .mocks.mock_api_model import MockApiModel
 
@@ -33,19 +34,20 @@ async def test_get_members_api() -> None:
     assert isinstance(result.member_data, list), "member_data should be a list"
 
 
+@pytest.mark.online
 @pytest.mark.asyncio
 async def test_get_sheets_api() -> None:
     fetcher = ApiAccessTokenFetcher(Post())
     token = await fetcher.fetch_access_token()
 
-    api = GetSheetsApi(token=token, sheet_id="27")
+    api = GetSheetsApi(token=token, sheet_id=27)
     api_executor = ApiExecutor(api)
 
     result = await api_executor.execute()
 
     assert result.id == 27, "id should be 27"
     assert result.name == "資格", "name should be '資格'"
-    assert result.record_type == 1, "record_type should be 1"
+    assert result.record_type == 0, "record_type should be 0"
     assert result.updated_at is not None, "updated_at should not be None"
     assert result.member_data is not None, "member_data should not be None"
     assert isinstance(result.member_data, list), "member_data should be a list"
